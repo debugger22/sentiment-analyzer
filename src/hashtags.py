afa536d3df810d2bc3378829e2436b14beb28ada
Sudhanshu <mrsud@jarvis.(none)>
2013-09-04 14:08:41 +0530

    Added docstrings in hashtags.py

--- src/hashtags.py
+++ src/hashtags.py
@@ -1,6 +1,8 @@
 from datalink import DatabaseConnectionDown
 class hashtags:
-
+	'''
+	This method extracts all the hashtags present in the tweet
+	'''
 	def extractHashtagTweet(self):
 		self.hashtagList = []
 		for i in self.tempTweet:
@@ -9,12 +11,18 @@ class hashtags:
 					self.hashtagList.append(i)
 			except:
 				pass
-
+	'''
+	This method extracts all the hashtags by calling extractHashtagTweet method and analyzes
+	them by searching for them in a database of hashtags
+	This method takes a string as parameter and returns a dictionary of the probabilities
+	of the sentiment about that string
+	DictKeys: 'positive', 'negative' 
+	'''
 	def analyseHashtagTweet(self, tweet):
 		self.tempTweet = tweet.split(" ")
 		self.extractHashtagTweet()
 		if len(self.hashtagList)==0:
-			return {'positive':0,'negative':0}
+			return {"positive":0,"negative":0}
 
 		fin = open("data/unigrams-pmilexicon.txt","r")
 		resultDict = {}
@@ -26,26 +34,17 @@ class hashtags:
 		for i in self.hashtagList:
 			for j in tempFile:
 				tempLine = j[:-1].split("\t")
-				#print tempLine[0]
 				if i != tempLine[0]:
 					continue
 				else:
-					#print tempLine
 					positiveNum = float(tempLine[2])
 					negativeNum = float(tempLine[3])
-					#print positiveNum,negativeNum	
 					positiveProb.append(positiveNum/(positiveNum+negativeNum))
 					negativeProb.append(negativeNum/(positiveNum+negativeNum))
-					#print positiveProb,negativeProb
-				
 		positiveProb = sum(positiveProb)/len(self.hashtagList)
 		negativeProb = sum(negativeProb)/len(self.hashtagList)
 		if positiveProb+negativeProb==0:
-			return {'positive':0,'negative':0}
+			return {"positive":0,"negative":0}
 		resultDict["positive"] = positiveProb
 		resultDict["negative"] = negativeProb
-		return resultDict 
-
-			
-
-
+		return resultDict
